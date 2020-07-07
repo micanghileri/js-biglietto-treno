25c39ea4ff70ec7563590364f8fa47ebdebfd487
Michele Anghileri <micheleanghileri@gmail.com>
2020-07-07 15:56:51 +0200

    sconto

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@ var eta = parseInt(prompt('Quanti anni hai?'));
 var costokm = km*0.21;
 
 console.log(costokm);
+
+if (eta<18) {
+    var scontomin = (costokm-((costokm*20)/100))
+} else if (eta>65) {
+    var scontoanz = (costokm-((costokm*40)/100))
+}
+
+console.log(scontomin, scontoanz);
