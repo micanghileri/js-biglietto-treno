b0a5e420cb22b617d60161490a30f6326fa2c9f0
Michele Anghileri <micheleanghileri@gmail.com>
2020-07-07 16:30:19 +0200

    console ceck done

--- js/app.js
+++ js/app.js
@@ -3,15 +3,18 @@ var eta = parseInt(prompt('Quanti anni hai?'));
 var tot = km*0.21;
 var percMin = 20;
 var percAnz = 40;
+var result;
 
 console.log(km);
 console.log(eta);
 console.log(tot);
 
+result = 'Nessuno sconto per te. Peccato. Tot. ' + tot + '€';
+
 if (eta<18) {
-    document.getElementById('costotck').innerHTML = tot-(tot*percMin)/100;
+    result = 'Hai meno di 18 anni. Super! Tot. ' + (tot-(tot*percMin)/100) + '€';
 } else if (eta>65) {
-    document.getElementById('costotck').innerHTML = tot-((tot*percAnz)/100);
-} else {
-    document.getElementById('costotck').innerHTML = tot;
+    result = 'L\'età non sempre è un limite. Eccoti uno sconto. Tot.' + (tot-(tot*percAnz)/100); + '€';
 }
+
+document.getElementById('costotck').innerHTML = result;
