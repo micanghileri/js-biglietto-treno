631b845ba6b2ac5293a75225e72cc19c8a069f46
Michele Anghileri <micheleanghileri@gmail.com>
2020-07-07 18:18:15 +0200

    definitivo aggiunto nome e cognome

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var name = prompt('Qual è il tuo nome?');
+var surname = prompt('Qual è il tuo cognome?');
 var km = parseInt(prompt('Quanti km vuoi percorrere?'));
 var eta = parseInt(prompt('Quanti anni hai?'));
 var tot = km*0.21;
@@ -13,6 +15,8 @@ if (eta<18) {
     result = (tot-(tot*percAnz)/100) + '€';
 }
 
-document.getElementById('costotck').innerHTML = result;
+document.getElementById('name-pass').innerHTML = name;
+document.getElementById('surname-pass').innerHTML = surname;
 document.getElementById('eta-pass').innerHTML = eta + ' anni';
 document.getElementById('km-pass').innerHTML = km + ' km';
+document.getElementById('costotck').innerHTML = result;
