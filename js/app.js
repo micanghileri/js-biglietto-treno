43bc1f3218dccd56e27b8c12db2fa6631f3febd9
Michele Anghileri <micheleanghileri@gmail.com>
2020-07-07 16:17:57 +0200

    funzionante

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var km = parseInt(prompt('Quanti km vuoi percorrere?'));
 var eta = parseInt(prompt('Quanti anni hai?'));
-var costokm = km*0.21;
+var tot = km*0.21;
+var percMin = 20;
+var percAnz = 40;
 
-console.log(costokm);
+console.log(km);
+console.log(eta);
+console.log(tot);
 
 if (eta<18) {
-    var scontomin = (costokm-((costokm*20)/100))
+    document.getElementById('costotck').innerHTML = tot-(tot*percMin)/100;
 } else if (eta>65) {
-    var scontoanz = (costokm-((costokm*40)/100))
+    document.getElementById('costotck').innerHTML = tot-((tot*percAnz)/100);
+} else {
+    document.getElementById('costotck').innerHTML = tot;
 }
-
-console.log(scontomin, scontoanz);
